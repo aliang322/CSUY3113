aff727583de5679822def11d47f29c3d1b9d69a4
Andrew S Liang <caliangandrew@gmail.com>
2020-07-26 05:41:31 -0700

    Project 5v1
    Implemented main menu and two levels with one enemy

---
+++ P5/SDLProject/Level1.cpp
@@ -0,0 +1,77 @@
+#include "Level1.h"
+
+#define LEVEL1_WIDTH 14
+#define LEVEL1_HEIGHT 8
+
+#define LEVEL1_ENEMY_COUNT 1
+
+unsigned int level1_data[] =
+{
+	3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3,
+	3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3,
+	3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3,
+	3, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 3,
+	3, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 3,
+	3, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
+	1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1,
+	2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2
+};
+
+void Level1::Initialize() {
+
+    state.nextScene = -1;
+
+	GLuint mapTextureID = Util::LoadTexture("tileset.png");
+	state.map = new Map(LEVEL1_WIDTH, LEVEL1_HEIGHT, level1_data, mapTextureID, 1.0f, 4, 1);
+	
+    state.player = new Entity();
+    state.player->entityType = PLAYER;
+    state.player->position = glm::vec3(2, -3, 0);
+    state.player->movement = glm::vec3(0);
+    state.player->acceleration = glm::vec3(0, -9.81f, 0);
+    state.player->speed = 4.0f;
+    state.player->textureID = Util::LoadTexture("george_0.png");
+
+    state.player->animRight = new int[4]{ 3, 7, 11, 15 };
+    state.player->animLeft = new int[4]{ 1, 5, 9, 13 };
+    state.player->animUp = new int[4]{ 2, 6, 10, 14 };
+    state.player->animDown = new int[4]{ 0, 4, 8, 12 };
+
+    state.player->animIndices = state.player->animRight;
+    state.player->animFrames = 4;
+    state.player->animIndex = 0;
+    state.player->animTime = 0;
+    state.player->animCols = 4;
+    state.player->animRows = 4;
+
+    state.player->height = 0.8f;
+    state.player->width = 0.8f;
+
+    state.player->jumpPower = 7.0f;
+
+    state.enemies = new Entity[LEVEL1_ENEMY_COUNT];
+
+    GLuint slimeTextureID = Util::LoadTexture("slime.png");
+    state.enemies[0].textureID = slimeTextureID;
+
+    for (int i = 0; i < LEVEL1_ENEMY_COUNT; i++) {
+        state.enemies[i].entityType = ENEMY;
+        state.enemies[i].speed = 2;
+    }
+
+    state.enemies[0].position = glm::vec3(8.0f, -4.75f, 0);
+    state.enemies[0].aiType = JUMPER;
+    state.enemies[0].acceleration = glm::vec3(0, -9.81f, 0);
+    state.enemies[0].aiState = IDLE;
+}
+
+void Level1::Update(float deltaTime) {
+    state.player->Update(deltaTime, state.player, state.enemies, LEVEL1_ENEMY_COUNT, state.map);
+
+    if (state.player->position.x >= 12) state.nextScene = 2;
+}
+
+void Level1::Render(ShaderProgram* program) {
+	state.map->Render(program);
+	state.player->Render(program);
+}
